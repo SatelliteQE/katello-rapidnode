3669275f4bc23dd060d073dbb173f5f3b4473c0e
Corey Welton <cwelton@redhat.com>
2014-07-09 18:26:12 -0400

    updated for the whole capsule paradigm vs nodes

--- katello-rapidnode.py
+++ katello-rapidnode.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 expandtab ai
 # katello-rapidnode.py 
-# this allows users to quickly enable and configure nodes for katello/satellite6
+# this allows users to quickly enable and configure capsules for katello/satellite6
 #
 # IMPORTANT NOTES:
 # * There is very little error checking presently existing in here. Patches 
@@ -37,7 +38,7 @@ def read_config_file():
 		else:
 			raise Exception, 'Invalid system type.'
 	if len(config_file_contents[0]) != 1:
-			raise Exception, 'Installation requires exactly "1" parent node, please check your config file.'
+			raise Exception, 'Installation requires exactly "1" parent instance, please check your config file.'
 	return config_file_contents
 
 def get_credentials_parent():
@@ -66,45 +67,82 @@ def paramiko_exec_command(system, username, password, command):
 
 def parent_get_oauth_secret(parent):
 # cat `/etc/katello/oauth_token-file`
-	data = []
-	username, password = get_credentials_parent()
-	command = "cat /etc/katello/oauth_token-file"
-	for results in paramiko_exec_command(parent, username, password, command):
-		data.append(results.strip())
-	oauth_secret = data[0]
-	return oauth_secret
+    oauth_data = []
+    username, password = get_credentials_parent()
+    #print colored("Grabbing oauth credentials from parent...", 'blue', attrs=['bold'])
+    # surely there are better ways to do this...
+    scrape_commands = ["grep oauth_consumer_key /etc/foreman/settings.yaml |sed 's/^:oauth_consumer_key: //'", 
+        "grep oauth_consumer_secret /etc/foreman/settings.yaml |sed 's/^:oauth_consumer_secret: //'", 
+        "grep oauth_secret /etc/pulp/server.conf |grep -v '#'| sed 's/^oauth_secret: //'"]
+    for scrape in scrape_commands:
+        data = []
+        command = scrape
+        for results in paramiko_exec_command(parent, username, password, command):
+            data.append(results.strip())
+        oauth_data.append(data[0])
+    return oauth_data
 
-def parent_gen_certs(parent, child):
-# node-certs-generate --child-fqdn <host> --katello-user admin --katello-password admin --katello-activation-key node
-	data = []
-	username, password = get_credentials_parent()
-	command = "node-certs-generate --child-fqdn " + child + " --katello-user admin --katello-password admin --katello-activation-key node"
-	print colored("Generating certs on parent...", 'blue', attrs=['bold'])
-	for results in paramiko_exec_command(parent, username, password, command):
-		print results.strip()
+def parent_gen_cert(parent, child):
+# capsule-certs-generate --capsule-fqdn <host> --certs-tar "<host>-certs.tar"
+    data = []
+    username, password = get_credentials_parent()
+    command = "capsule-certs-generate -v --capsule-fqdn " + child + " --certs-tar " + child + "-certs.tar"
+    print colored("Generating certs on parent...", 'blue', attrs=['bold'])
+    for results in paramiko_exec_command(parent, username, password, command):
+        print results.strip()
+
+# Do these two until I figure out a convenient way to do ssh-keys
+# 1)
+def parent_copy_cert_local(parent, child):
+    certs_file = child + "-certs.tar"
+    port = 22
+    username, password = get_credentials_parent()
+    transport = paramiko.Transport((parent, port))
+    transport.connect(username=username, password=password)
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    print colored("Retrieving certs file from parent...", 'blue', attrs=['bold'])
+    sftp.get(certs_file, certs_file)
+    sftp.close()
+
+# 2)
+def child_copy_cert(child):
+    certs_file = child + "-certs.tar"
+    port = 22
+    username, password = get_credentials_children()
+    transport = paramiko.Transport((child, port))
+    transport.connect(username=username, password=password)
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    print colored("Pushing certs to child...", 'blue', attrs=['bold'])
+    sftp.put(certs_file, certs_file)
+    sftp.close()
 
 def child_register(parent, child):
 # download cert
 	data = []
 	cmds = []
 	username, password = get_credentials_children()
-	parent_satcert = "http://" + parent +"/pub/candlepin-cert-consumer-latest.noarch.rpm"
+	parent_satcert = "http://" + parent +"/pub/katello-ca-consumer-latest.noarch.rpm"
 	installrpm = "rpm -Uvh " + parent_satcert
 # subscription-manager
-	register = "subscription-manager register --org Katello_Infrastructure --activationkey node --force"
+	register = "subscription-manager register --username admin --password changeme --org ACME_Corporation --environment 'dev/mycv'  --auto-attach --force"
 	cmds = installrpm, register
-	print colored("Registering child to parent node...", 'blue', attrs=['bold'])
+	print colored("Registering/subscribing child to parent...", 'blue', attrs=['bold'])
 	for command in cmds:
 		for results in paramiko_exec_command(child, username, password, command):
 			print results.strip()
 
-def child_init_node(parent, child):
-	username, password = get_credentials_children()
-	command = "node-install -v --parent-fqdn " + parent +" --pulp true --pulp-oauth-secret " \
-			+ oauth_secret + " --puppet true --puppetca true --foreman-oauth-secret " \
-			+ oauth_secret +  " --register-in-foreman true"
-	print colored("Configuring child node...", 'blue', attrs=['bold'])
-	for results in paramiko_exec_command(child, username, password, command):
+def child_capsule_init(parent, child):
+    username, password = get_credentials_children()
+    foreman_oauth_key, foreman_oauth_secret, pulp_oauth_secret = parent_get_oauth_secret(parent)
+    certs_tar = child + "-certs.tar"
+    command = "capsule-installer -v --certs-tar " + certs_tar + " --parent-fqdn " \
+            + parent +" --pulp true --pulp-oauth-secret " \
+			+ pulp_oauth_secret + " --puppet true --puppetca true --foreman-oauth-secret " \
+			+ foreman_oauth_secret +  " --foreman-oauth-key " \
+            + foreman_oauth_key + " --register-in-foreman true"
+#    print command
+    print colored("Configuring child capsule (this may take a while)...", 'blue', attrs=['bold'])
+    for results in paramiko_exec_command(child, username, password, command):
 		print results.strip()
 
 def child_copy_repo(child):
@@ -122,11 +160,11 @@ def child_copy_repo(child):
 	sftp.put(repo_file, remote_repo_file)
 	sftp.close()
 
-def child_nodeinstaller_v8(child):
+def child_capsule_installer(child):
 	data = []
 	username, password = get_credentials_children()
-	command = "yum -y install node-installer v8"
-	print colored("Installing node-installer and v8...\n", 'blue', attrs=['bold'])
+	command = "yum -y install katello-installer"
+	print colored("Installing capsule-installer...\n", 'blue', attrs=['bold'])
 	for results in paramiko_exec_command(child, username, password, command):
 		data.append(results)
 
@@ -144,54 +182,86 @@ def child_disable_selinux(child):
 # `katello --user admin --password admin node list`
 # to assure our nodes are online
 
-def parent_get_org_environments():
-# katello -u admin -p admin environment list --org "Katello Infrastructure" -g -d :
-	data = []
-	newdata = []
-	record = []
-	environments =  []
-	username, password = get_credentials_parent()
-	command = "katello -u admin -p admin environment list --org 'Katello Infrastructure' -g -d :"
-	print colored("Determining environments in org on parent node...\n", 'blue', attrs=['bold'])
-	for results in paramiko_exec_command(parent, username, password, command):
-		data.append(results)
-# Basically screen-scraping. What a hassle! is there a better way?
-	data = data[0].split("\n")
-	data = data[5:]
-	data.pop(-1)
-	for envdata in data:
-		record = envdata.split(':')
-		environments.append(record[1])
-	return environments
+def parent_get_org_environments(capsule_id):
+    data = []
+    record = []
+    environments = []
+    username, password = get_credentials_parent()
+    command = "hammer --output csv capsule content available-lifecycle-environments --id " + capsule_id
+    for results in paramiko_exec_command(parent, username, password, command):
+        data.append(results)
+    # Basically screen-scraping. What a hassle! is there a better way?
+    environments = data[0].split("\n")
+    environments.pop()
+    environments.pop(0)
+    return environments
 
-def parent_populate_child_environments(parent, child):
-# katello -u admin -p admin node add_environment --environment dev --org "Katello Infrastructure" --id 5
-	data =[]
-	environments = parent_get_org_environments()
-        username, password = get_credentials_parent()
-	print colored("Populating child node with environments...", 'blue', attrs=['bold'])
-	for env in environments:
-		command = "katello -u admin -p admin node add_environment --environment \"" \
-		+ env + "\" --org \"Katello Infrastructure\" --name " + child
-		print colored('[' + env + ']', 'cyan')
-		for results in paramiko_exec_command(parent, username, password, command):
-			print results.strip()
+def parent_get_capsules():
+    data = []
+    record = []
+    capsule_ids = []
+    capsule_names = []
+    username, password = get_credentials_parent()
+    command = "hammer --output csv capsule list"
+    for results in paramiko_exec_command(parent, username, password, command):
+        data.append(results)
+    # Once again...
+    capsules = data[0].split("\n")
+    capsules.pop()
+    capsules.pop(0)
+    return capsules
 
-#def parent_sync_child(parent, child):
-# TODO: Initiate syncing of child
-# (waiting for stability assurance)
+def populate_capsules(parent, child):
+# For now this needs to be run after ALL capsules have been created.
+# This is because all content pushes are currently done via capsule id.
+# It is very difficult to associate a capsule id with the capsule name 
+# we have provided at the beginning and have it make sense visually.
+#
+# IOW we can only sync by 'id', not by the 'hostname' users provide in
+# the config settings.
+#
+# If there exists a way to simply perform all the 'capsule content' 
+# functions via capsule name vs id, this can be easily remedied later.
+
+    data = []
+    print colored("Determining all capsules...\n", 'blue', attrs=['bold'])
+    capsules = parent_get_capsules()
+    username, password = get_credentials_parent()
+    print colored("Populating child capsule with environments...", 'blue', attrs=['bold'])
+#    print capsules
+    for cap in capsules:
+        capsule_id, capsule_name, capsule_url = cap.split(",")
+        print colored("Populating capsule:", 'white', attrs=['bold', 'underline'])
+        print colored(capsule_name, 'cyan', attrs=['bold'])
+        # Don't try to do anything to default capsule
+        if capsule_id != "1":i
+            print colored("Determining applicable environments for capsule...\n", 'blue', attrs=['bold'])
+            environments = parent_get_org_environments(capsule_id)
+            for env in environments:
+                env_id, env_name, env_org = env.split(",")
+                print colored('[' + env_org + '/' + env_name + ']', 'cyan')
+                command = "hammer capsule content add-lifecycle-environment --environment-id " \
+                    + env_id + " --id " + capsule_id
+                for results in paramiko_exec_command(parent, username, password, command):
+                    print results.strip()
+            sync_command = "hammer capsule content synchronize --async --id " + capsule_id
+            for results in paramiko_exec_command(parent, username, password, sync_command):
+                print results.strip()
 
 satellite_systems = read_config_file()
 parent = satellite_systems[0][0]
-oauth_secret = parent_get_oauth_secret(parent)
-
 for child in satellite_systems[1]:
-	print colored("Configuring node:", 'white', attrs=['bold', 'underline']) 
-	print colored(child, 'cyan', attrs=['bold'])
-	parent_gen_certs(parent, child)
-	child_copy_repo(child)
-	child_nodeinstaller_v8(child)
-	child_register(parent, child)
-	child_disable_selinux(child)
-	child_init_node(parent, child)
-	parent_populate_child_environments(parent, child)
+    print colored("Configuring capsule:", 'white', attrs=['bold', 'underline']) 
+    print colored(child, 'cyan', attrs=['bold'])
+    parent_gen_cert(parent, child)
+    child_copy_repo(child)
+    child_register(parent, child)
+    child_disable_selinux(child)
+    parent_copy_cert_local(parent, child)
+    child_copy_cert(child)
+    child_capsule_installer(child)
+    child_capsule_init(parent, child)
+
+# After configuration is complete, populate environments (and eventually content)
+# for ALL capsules
+populate_capsules(parent, child)
